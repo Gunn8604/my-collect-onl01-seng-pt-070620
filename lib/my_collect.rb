59b58dc43495ac058cc26039472b73d6fd105380
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-19 04:12:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,20 @@
 def my_collect(array)
   if array.size == 0
-end
+    "Array is empty"
+  end
+  i = 0 
+  temp_array = []
+  while i < array.size
+    if array[i].include?(" ")
+      temp_array.push()
+      #execute code
+      i = i + 1
+    else 
+      temp_array.push()
+      i = i + 1 
+    end
+      
+    end
+  end
 end
 
